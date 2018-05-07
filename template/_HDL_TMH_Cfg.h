f65b2212ef9927d2d3545566f12235b0340c2aa8
Alvin Yee <changchang.ye@hotmail.com>
2018-05-07 10:30:38 +0800

    Create _HDL_TMH_Cfg.h

---
+++ template/_HDL_TMH_Cfg.h
@@ -0,0 +1,26 @@
+/*
+ * _HDL_TMH_Cfg.h
+ *
+ *  Created on: 20th,Feb,2018
+ *      Author: yech
+ */
+/*[[[cog
+print('    [info]: handling template _HDL_TMH_Cfg.h starts')
+import sys
+sys.path.append('../')
+sys.path.append('../../')
+import cog
+from PkMqXmlParser import MdMqxmlParser
+xmlFile = r'C:\workspace\CocoNut\description\Coco.xml'
+mqxml = MdMqxmlParser.MqXmlParser(xmlFile)
+]]]*/
+//[[[end]]]
+#ifndef HDL_TMH_CFG_H_
+#define HDL_TMH_CFG_H_
+
+
+/*[[[cog
+print('    [info]: handling template _HDL_TMH_Cfg.h ends')
+]]]*/
+//[[[end]]]
+#endif /* HDL_TMH_CFG_H_ */
